c99faf1a98f04997253a7d0c48d547de72eadda0
Jeff Reiner <theundeadghost@gmail.com>
2022-11-25 18:25:23 +0100

    Little bit of a cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
-import { resolve } from "path";
-
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "vite-typescript-library-starter",
-      filename: "vite-typescript-library-starter",
+      entry: "src/index.ts",
+      name: "vite-ts-library-starter",
+      fileName: "vite-ts-library-starter",
     },
     sourcemap: true,
-  },
-  rollupOptions: {
-    // make sure to externalize deps that shouldn't be bundled
-    // into your library
-    external: [],
-    output: {
-      // Provide global variables to use in the UMD build
-      // for externalized deps
-      globals: {},
-    },
   },
 });
